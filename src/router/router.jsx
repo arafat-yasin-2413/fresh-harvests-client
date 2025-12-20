e19d2828d700da4f7ed9b5a92a6b2cc28c4127f2
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-20 22:00:33 +0600

    initial router and other setup is done

---
+++ src/router/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <div><h2>Hello world</h2></div>,
+    },
+]);
