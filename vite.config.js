2d7997e3d27799fcc4720ce947385f387896acc9
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-12-23 21:28:50 +0600

    deployment settings updated

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,40 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+// 	plugins: [react(), tailwindcss()],
+//     server: {
+//         proxy: {
+//             "/api":{
+//                 target: "http://23.239.111.164:5001",
+//                 changeOrigin: true,
+//                 secure: false,
+//             },
+//         },
+//     },
+// });
+
+
+
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tailwindcss()],
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
     server: {
-        proxy: {
-            "/api":{
-                target: "http://23.239.111.164:5001",
-                changeOrigin: true,
-                secure: false,
-            },
+      proxy: {
+        "/api/v1": {
+          target: env.VITE_API_URL || "http://23.239.111.164:5001",
+          changeOrigin: true,
         },
+      },
     },
-});
+  });
+};
